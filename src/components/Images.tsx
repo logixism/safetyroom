fee93b4fa7e40ba97fed18663e7ac65e6c9f6907
logix <logixism@pm.me>
2024-11-08 12:39:00 +0200

    Update Images.tsx

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -10,32 +10,33 @@ import LeftSide from "/public/assets/hires/rooms/left_side.png";
 import LeftSide_Lowres from "/public/assets/hires/rooms/left_side.png";
 
 export const Images = ({ room }: { room: string }) => {
-  function makeElement(image: any, lowres_image: any, room: string) {
-    const [isLoaded, setIsLoaded] = useState(false);
-  
-    return (<>
-      <Image
-        src={lowres_image}
-        alt={room}
-        className={`w-full h-full absolute top-0 left-0 pointer-events-none blur-md transition-all duration-900 ease-in-out z-10 ${
-          isLoaded ? "opacity-0 blur-none" : "opacity-100"
-        }`}
-      />
-      <Image
-        className="w-full h-full absolute top-0 left-0 pointer-events-none"
-        src={image}
-        alt={room}
-        onLoad={() => setIsLoaded(true)}
-      />
-    </>)
-  }
+
+  let lowres_image: any;
+  let image: any;
 
   switch (room) {
     case "entrance":
-      return makeElement(Entrance, Entrance_Lowres, room);
+      lowres_image = Entrance_Lowres;
+      image = Entrance;
     case "left_side":
-      return makeElement(LeftSide, LeftSide_Lowres, room);
-    default:
-      return <></>
+      lowres_image = LeftSide_Lowres;
+      image = LeftSide;
   }
+
+  const [isLoaded, setIsLoaded] = useState(false);
+  return (<>
+    <Image
+      src={lowres_image}
+      alt={room}
+      className={`w-full h-full absolute top-0 left-0 pointer-events-none blur-md transition-all duration-900 ease-in-out z-10 ${
+        isLoaded ? "opacity-0 blur-none" : "opacity-100"
+      }`}
+    />
+    <Image
+      className="w-full h-full absolute top-0 left-0 pointer-events-none"
+      src={image}
+      alt={room}
+      onLoad={() => setIsLoaded(true)}
+    />
+  </>)
 };
