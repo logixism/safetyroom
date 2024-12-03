953351e48bc309fe41762b0b35d83e2b62bcdf7b
logix <logixism@pm.me>
2024-12-03 14:50:52 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,19 +19,19 @@ export default function Home() {
           width={0}
           height={0}
           sizes="100vw"
-          className="m-auto blinking-image transition-opacity"
+          className="m-auto blinking-image transition-opacity rounded-3xl "
           style={{ width: "50%", height: "auto" }}
         />
+        <button
+          className="relative bottom-16 left-1/2 -translate-x-1/2 bg-green-500 px-4 py-2 rounded-md text-xl font-bold transition-all duration-300 hover:bg-green-400 text-white"
+          onClick={() => {
+            router.push("/rooms/entrance");
+            document.body.requestFullscreen();
+          }}
+        >
+          Почати пригоду
+        </button>
       </div>
-      <button
-        className="absolute left-1/2 -translate-x-1/2 bottom-60 bg-green-500 px-4 py-2 rounded-md text-xl font-bold transition-all duration-300 hover:bg-green-400 text-white"
-        onClick={() => {
-          router.push("/rooms/entrance");
-          document.body.requestFullscreen();
-        }}
-      >
-        Почати пригоду
-      </button>
     </>
   );
 }
