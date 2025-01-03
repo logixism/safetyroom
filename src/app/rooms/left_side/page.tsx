f7d16246c9153f1ebe793a61ee2b3d01fe271e0e
logix <logixism@pm.me>
2025-01-03 14:41:07 +0200

    asd

--- src/app/rooms/left_side/page.tsx
+++ src/app/rooms/left_side/page.tsx
@@ -1,133 +1,180 @@
 import { Arrow, ArrowProps } from "@/components/Arrow";
 import { PulsingCircle, PulsingCircleProps } from "@/components/PulsingCircle";
 import Images from "@/components/Images";
+import PdfView from "@/components/PdfView";
+let pdf_minefolder = <PdfView file="../assets/files/mines_bluefolder.pdf" />;
 
 const arrows: ArrowProps[] = [
-  {
-    direction: "right",
-    text: "Подивитися праворуч",
-    href: "/rooms/entrance",
-    directionPercentage: 5,
-    top: 50,
-  },
+    {
+        direction: "right",
+        text: "Подивитися праворуч",
+        href: "/rooms/entrance",
+        directionPercentage: 5,
+        top: 50,
+    },
 ];
 
 const circles: PulsingCircleProps[] = [
-  {
-    content: (
-      <>
-        <img src="../assets/hires/signs/premedical_help.png" alt="" />
-      </>
-    ),
-    text: "Домедична допомога",
-    top: 24.5,
-    left: 13.5,
-  },
-  {
-    content: (
-      <>
-        <img src="../assets/hires/signs/premedical_booklet_1.png" alt="" />
-      </>
-    ),
-    text: "Перший буклет",
-    top: 55.5,
-    left: 6.2,
-    radius: 5,
-  },
-  {
-    content: (
-      <>
-        <img src="../assets/hires/signs/premedical_booklet_2.png" alt="" />
-      </>
-    ),
-    text: "Другий буклет",
-    top: 55.5,
-    left: 13.7,
-    radius: 5,
-  },
-  {
-    content: (
-      <>
-        <img src="../assets/hires/signs/premedical_booklet_3.png" alt="" />
-      </>
-    ),
-    text: "Третій буклет",
-    top: 55.5,
-    left: 21.1,
-    radius: 5,
-  },
+    {
+        content: (
+            <>
+                <img src="../assets/hires/signs/premedical_help.png" alt="" />
+            </>
+        ),
+        text: "Домедична допомога",
+        top: 24.5,
+        left: 13.5,
+    },
+    {
+        content: (
+            <>
+                <img
+                    src="../assets/hires/signs/premedical_booklet_1.png"
+                    alt=""
+                />
+            </>
+        ),
+        text: "Перший буклет",
+        top: 55.5,
+        left: 6.2,
+        radius: 5,
+    },
+    {
+        content: (
+            <>
+                <img
+                    src="../assets/hires/signs/premedical_booklet_2.png"
+                    alt=""
+                />
+            </>
+        ),
+        text: "Другий буклет",
+        top: 55.5,
+        left: 13.7,
+        radius: 5,
+    },
+    {
+        content: (
+            <>
+                <img
+                    src="../assets/hires/signs/premedical_booklet_3.png"
+                    alt=""
+                />
+            </>
+        ),
+        text: "Третій буклет",
+        top: 55.5,
+        left: 21.1,
+        radius: 5,
+    },
 
-  {
-    content: (
-      <>
-        <img src="../assets/hires/signs/psychological_relief.png" alt="" />
-      </>
-    ),
-    text: "Психологічні розвантаження",
-    top: 25,
-    left: 48.6,
-  },
-  {
-    content: (
-      <>
-        <img src="../assets/hires/signs/psychological_booklet_1.png" alt="" />
-      </>
-    ),
-    text: "Перший буклет",
-    top: 55.5,
-    left: 42.4,
-    radius: 5,
-  },
-  {
-    content: (
-      <>
-        <img src="../assets/hires/signs/psychological_booklet_2.png" alt="" />
-      </>
-    ),
-    text: "Другий буклет",
-    top: 55.5,
-    left: 51.5,
-    radius: 5,
-  },
+    {
+        content: (
+            <>
+                <img
+                    src="../assets/hires/signs/psychological_relief.png"
+                    alt=""
+                />
+            </>
+        ),
+        text: "Психологічні розвантаження",
+        top: 25,
+        left: 48.6,
+    },
+    {
+        content: (
+            <>
+                <img
+                    src="../assets/hires/signs/psychological_booklet_1.png"
+                    alt=""
+                />
+            </>
+        ),
+        text: "Перший буклет",
+        top: 55.5,
+        left: 42.4,
+        radius: 5,
+    },
+    {
+        content: (
+            <>
+                <img
+                    src="../assets/hires/signs/psychological_booklet_2.png"
+                    alt=""
+                />
+            </>
+        ),
+        text: "Другий буклет",
+        top: 55.5,
+        left: 51.5,
+        radius: 5,
+    },
 
-  {
-    content: (
-      <>
-        <img src="../assets/hires/signs/mine_safety.png" alt="" />
-      </>
-    ),
-    text: "Мінна безпека",
-    top: 26,
-    left: 83.7,
-  },
-  {
-    content: (
-      <>
-        <img src="../assets/hires/signs/mine_booklet.png" alt="" />
-      </>
-    ),
-    text: "Буклет",
-    top: 55.5,
-    left: 82,
-    radius: 5,
-  },
+    {
+        content: (
+            <>
+                <img src="../assets/hires/signs/mine_safety.png" alt="" />
+            </>
+        ),
+        text: "Мінна безпека",
+        top: 26,
+        left: 83.7,
+    },
+    {
+        content: (
+            <>
+                <img src="../assets/hires/signs/mine_booklet.png" alt="" />
+            </>
+        ),
+        text: "Буклет",
+        top: 55.5,
+        left: 82,
+        radius: 5,
+    },
+    {
+        content: (
+            <>
+                <div>
+                    {[...Array(8).keys()].map((i) => (
+                        <img
+                            key={i}
+                            src={`../assets/hires/signs/minebox_${i + 1}.png`}
+                            alt={`Буклет ${i + 1}`}
+                            className="w-full"
+                        />
+                    ))}
+                </div>
+            </>
+        ),
+        text: "Мінний Бокс",
+        top: 65.5,
+        left: 92,
+        radius: 5,
+    },
+    {
+        content: <>{pdf_minefolder}</>,
+        text: "Мінна папка",
+        top: 65.5,
+        left: 79,
+        radius: 5,
+    },
 ];
 
 const FrontPage = () => {
-  return (
-    <>
-      <Images room="left_side" />
-      <div className="absolute w-full h-full top-0 left-0">
-        {arrows.map((arrow, i) => (
-          <Arrow key={i} {...arrow} />
-        ))}
+    return (
+        <>
+            <Images room="left_side" />
+            <div className="absolute w-full h-full top-0 left-0">
+                {arrows.map((arrow, i) => (
+                    <Arrow key={i} {...arrow} />
+                ))}
 
-        {circles.map((circle, i) => (
-          <PulsingCircle key={i} {...circle} />
-        ))}
-      </div>
-    </>
-  );
+                {circles.map((circle, i) => (
+                    <PulsingCircle key={i} {...circle} />
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default FrontPage;
--- src/components/PulsingCircle.tsx
+++ src/components/PulsingCircle.tsx
@@ -3,47 +3,61 @@
 import { useState, ReactNode } from "react";
 
 export interface PulsingCircleProps {
-  onClick?: () => void;
-  onClose?: () => void;
-  content: ReactNode;
-  text: string;
-  top: number;
-  left: number;
-  radius?: number;
+    onClick?: () => void;
+    onClose?: () => void;
+    content: ReactNode;
+    text: string;
+    top: number;
+    left: number;
+    radius?: number;
 }
 
+const texts = [
+    "А що тут?",
+    "А тут що?",
+    "Подивись сюди!",
+    "Що це таке?",
+    "Дивись!",
+    "Ти бачив це?",
+    "Це цікаво!",
+    "Тут щось є!",
+    "Не пропусти!",
+];
+
 export const PulsingCircle = ({
-  onClick,
-  onClose,
-  content,
-  text,
-  top,
-  left,
-  radius,
+    onClick,
+    onClose,
+    content,
+    text,
+    top,
+    left,
+    radius,
 }: PulsingCircleProps) => {
-  const [showImage, setShowImage] = useState(false);
+    const [showImage, setShowImage] = useState(false);
 
-  const handleClick = () => {
-    setShowImage(true);
-  };
+    text = texts[Math.floor(Math.random() * texts.length)];
 
-  const handleClose = () => {
-    setShowImage(false);
-    onClose?.();
-  };
+    const handleClick = () => {
+        setShowImage(true);
+    };
 
-  return (
-    <>
-      <div
-        className="group absolute cursor-pointer flex flex-col items-center justify-center z-20"
-        style={{ left: `${left}%`, top: `${top}%` }}
-        onClick={() => {
-          handleClick();
-          onClick?.();
-        }}
-      >
-        <div
-          className={`
+    const handleClose = () => {
+        setShowImage(false);
+        onClose?.();
+    };
+
+    return (
+        <>
+            <div
+                className="group absolute cursor-pointer flex flex-col items-center justify-center z-20"
+                style={{ left: `${left}%`, top: `${top}%` }}
+                onClick={() => {
+                    handleClick();
+                    onClick?.();
+                }}
+            >
+                <div
+                    className={`
           circle 
           ${radius ? `w-${radius} h-${radius}` : "w-8 h-8"}
           rounded-full 
@@ -53,46 +67,46 @@ export const PulsingCircle = ({
           group-hover:opacity-0 
           group-hover:-z-10
         `}
-        >
-          <span className="block absolute h-full w-full rounded-full bg-white/10 border border-white opacity-75 animate-ping-xl"></span>
-        </div>
-        <div
-          className="link flex flex-col items-center justify-center opacity-0 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100 group-hover:-mt-4 transition-all duration-500 ease-in-out"
-          style={{ position: "absolute", top: "100%" }}
-        >
-          <span className="-mb-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="w-5 h-5"
-              fill="#ffffff"
+                >
+                    <span className="block absolute h-full w-full rounded-full bg-white/10 border border-white opacity-75 animate-ping-xl"></span>
+                </div>
+                <div
+                    className="link flex flex-col items-center justify-center opacity-0 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100 group-hover:-mt-4 transition-all duration-500 ease-in-out"
+                    style={{ position: "absolute", top: "100%" }}
+                >
+                    <span className="-mb-1">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            className="w-5 h-5"
+                            fill="#ffffff"
+                        >
+                            <path d="M24 22h-24l12-20z" />
+                        </svg>
+                    </span>
+                    <div className="bg-white px-4 py-1 rounded-lg font-bold">
+                        <span className="text text-nowrap">{text}</span>
+                    </div>
+                </div>
+            </div>
+            <div
+                className={`z-30 fixed top-0 left-0 w-full h-full bg-white/10 flex items-center justify-center backdrop-blur-md transition-opacity duration-200 ${
+                    showImage ? "opacity-100" : "opacity-0 pointer-events-none"
+                }`}
+                onClick={handleClose}
             >
-              <path d="M24 22h-24l12-20z" />
-            </svg>
-          </span>
-          <div className="bg-white px-4 py-1 rounded-lg font-bold">
-            <span className="text text-nowrap">{text}</span>
-          </div>
-        </div>
-      </div>
-      <div
-        className={`z-30 fixed top-0 left-0 w-full h-full bg-white/10 flex items-center justify-center backdrop-blur-md transition-opacity duration-200 ${
-          showImage ? "opacity-100" : "opacity-0 pointer-events-none"
-        }`}
-        onClick={handleClose}
-      >
-        <div className="fixed inset-0 bg-black/50 flex justify-center items-center">
-          <div
-            className="bg-white p-4 rounded-2xl max-w-5xl min-w-fit max-h-[95vh] overflow-y-scroll"
-            onClick={(e) => e.stopPropagation()}
-          >
-            {content}
-            {/* {showImage && content} */}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                <div className="fixed inset-0 bg-black/50 flex justify-center items-center">
+                    <div
+                        className="bg-white p-4 rounded-2xl max-w-5xl min-w-fit max-h-[95vh] overflow-y-scroll"
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        {content}
+                        {/* {showImage && content} */}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
